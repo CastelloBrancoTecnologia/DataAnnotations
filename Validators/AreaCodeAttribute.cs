ee9b3443239779cf49bb7158b8c15141baf8bcd4
Cesar Castello Branco <cesar@castellobranco.tec.br>
2024-10-07 21:51:16 -0300

    updates

--- Validators/AreaCodeAttribute.cs
+++ Validators/AreaCodeAttribute.cs
@@ -10,15 +10,20 @@ using CastelloBranco.Singletons;
 namespace CastelloBranco.DataAnnotations;
 
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
-public class AreaCode : ValidationAttribute
+public class AreaCodeAttribute : ValidationAttribute
 {
-    public AreaCode() : base(SharedResources.DDDInvalido)
+    public AreaCodeAttribute() : base(SharedResources.DDDInvalido)
     {
     }
 
-    public AreaCode(string ErrorMessage) : base(ErrorMessage) { }
+    public AreaCodeAttribute(string ErrorMessage) : base(ErrorMessage) { }
 
     public bool AllowEmpty { get; set; }
+
+    private const string BrazilCountryCode = "55";
+
+    public string CountryCode { get; set; } = BrazilCountryCode;
+
 
     public override bool IsValid(object? value)
     {
@@ -35,17 +40,23 @@ public class AreaCode : ValidationAttribute
             return AllowEmpty;
         }
 
-        if (stringValue.Length != 2)
+        if (CountryCode == BrazilCountryCode && stringValue.Length != 2)
+        {
+            return false;
+        }
+
+        if ( (CountryCode != BrazilCountryCode) && (stringValue.Length < 2 || stringValue.Length > 3) )
         {
             return false;
         }
 
         if (! (char.IsDigit(stringValue[0])) ||
-            ! (char.IsDigit(stringValue[1])) )
+            ! (char.IsDigit(stringValue[1])) ||
+            ! (stringValue.Length == 3 && char.IsDigit(stringValue[2])))
         {
             return false;
         }
 
-        return BrazilAreaCode.AreaCodes.Any(d => d.AreaCode == stringValue);
+        return (CountryCode != BrazilCountryCode) || BrazilAreaCode.AreaCodes.Any(d => d.AreaCode == stringValue);
     }
 }
